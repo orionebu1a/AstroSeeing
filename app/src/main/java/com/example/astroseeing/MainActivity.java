9b27e16ac38e1e3069422d6622a4eaa6f8c27d77
orionebu1a <92534795+orionebu1a@users.noreply.github.com>
2023-03-18 22:48:06 +0300

    first

---
+++ app/src/main/java/com/example/astroseeing/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.astroseeing;
+
+import static android.content.ContentValues.TAG;
+import static androidx.navigation.ActivityKt.findNavController;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bot_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        openFragment(Home.newInstance("",""));
+                        return true;
+                    case R.id.navigation_seeing:
+                        openFragment(Light_polution.newInstance("",""));
+                        return true;
+                    case R.id.navigation_light_pollution:
+                        openFragment(Seeing.newInstance("",""));
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+    private void openFragment(Fragment fragment) {
+        Log.d(TAG, "openFragment: ");
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //this is a helper class that replaces the container with the fragment. You can replace or add fragments.
+        transaction.replace(R.id.fragmentContainerView2, fragment);
+        transaction.addToBackStack(null); //if you add fragments it will be added to the backStack. If you replace the fragment it will add only the last fragment
+        transaction.commit(); // commit() performs the action
+    }
+}
